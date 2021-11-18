61cb16604216dcb35afa67404ef40f0ce5dcd4a1
Antoni Papiewski <antekpapiewski@gmail.com>
2021-11-18 11:29:07 +0100

    remove obsolete circular progress

--- src/components/UI/CircularProgress.tsx
+++
@@ -1,7 +0,0 @@
-import circularProgressGif from "../../assets/circular-progress.gif";
-
-export function CircularProgress() {
-  return (
-    <img src={circularProgressGif} alt="Loading" className="flex-grow-0" />
-  );
-}
